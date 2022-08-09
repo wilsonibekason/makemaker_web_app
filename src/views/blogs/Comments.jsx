325bc728db5b2be728284ccd3b871c1f19d66d47
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-09 16:47:49 +0100

    complete comment ui component

--- src/views/blogs/Comments.jsx
+++ src/views/blogs/Comments.jsx
@@ -3,22 +3,29 @@ import React from "react";
 const Comments = () => {
   return (
     <>
-      <div className="relative flex flex-col min-w-0 break-words w-1/2 mb-6 shadow-lg rounded-lg bg-white my-4 mx-auto">
-        <div className="flex items-start p-3 border-b border-solid border-blueGray-200 rounded-t ">
-          <h1 className="text-xs mx-4 text-blueGray-400 font-semibold">
-            {` wilson ibekason .`}
-          </h1>
-          <span className="text-xs text-blueGray-400 font-semibold">
-            20/8/2005
-          </span>
-        </div>
-        <div className="flex items-start justify-between p-3 border-b border-solid border-blueGray-200 rounded-t">
-          <h1 className="text-xs text-lightBlue-400 font-semibold  w-4/12">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla,
-            nostrum quos. Velit quia, nesciunt temporibus atque nulla quo magnam
-            ratione!
-          </h1>
-          <span className="text-xs">jjjjj</span>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mb-8 gap-3 px-2">
+        {/* <div className="relative flex flex-col min-w-0 break-words w-1/2 mb-6 shadow-lg rounded-lg bg-white my-4 mx-auto"> */}
+        <div className="max-w-lg px-8 py-4 mx-auto shadow-lg rounded-lg bg-white">
+          <div className="flex items-start p-3 border-b border-solid border-gray-200 rounded-t ">
+            <h1 className="text-xs mx-4 text-gray-400 font-semibold w-1/2">
+              wilson ibekason
+            </h1>
+            <span className="text-xs text-gray-400 font-semibold">
+              20/8/2005
+            </span>
+          </div>
+          <div className="flex items-start justify-between p-3 border-b border-solid border-gray-200 rounded-t">
+            <div className="w-5/6">
+              <h1 className="text-xs text-blue-400 font-semibold">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla,
+                nostrum quos. Velit quia, nesciunt temporibus atque nulla quo
+                magnam ratione!
+              </h1>
+            </div>
+            <div className="1/6">
+              <span className="text-xs">jjjjj</span>
+            </div>
+          </div>
         </div>
       </div>
     </>
