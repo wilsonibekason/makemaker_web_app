8d39fb0f45f3d20eabbb9919e49b1c905d1cad54
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-10 18:23:11 +0100

    edit blogcomment schema

--- makemaker/schemas/blogs/blogComments.js
+++ makemaker/schemas/blogs/blogComments.js
@@ -9,37 +9,19 @@ export default {
     {
       name: "fullName",
       title: "FullName",
-      type: "text",
+      type: "string",
       required: true,
-      display: {
-        placeholder: "Enter Name",
-        rules: [
-          {
-            required: true,
-            message: "Please enter a name",
-          },
-        ],
-      },
     },
     {
       name: "email",
       title: "Email",
       type: "email",
       required: true,
-      display: {
-        placeholder: "Enter Email",
-        rules: [
-          {
-            required: true,
-            message: "Please enter a valid email address",
-          },
-        ],
-      },
     },
     {
       name: "title",
       title: "Blog Title",
-      type: "text",
+      type: "string",
       display: {
         placeholder: "Enter Your Comment Title",
         rules: [
